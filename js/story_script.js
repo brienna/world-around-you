3e4fc8a2602207321832ded81d9672ada2db525a
Brienna Herold <brienna.herold@gmail.com>
2018-11-06 21:38:28 -0500

    Polish menu button styling

--- js/story_script.js
+++ js/story_script.js
@@ -148,21 +148,26 @@ window.onload = function() {
         // For each menu button
         for (var i = 0; i < menuBtns.length; i++) {
             // Add an event handler
-            menuBtns[i].addEventListener('mouseover', function(e) {
-                // Get data for the menu element to use as description
-                description.textContent = this.getAttribute('data');
-                // Position & show description
-                description.style.visibility = 'visible'; // quickly show then...
-                $(description).css({
-                    'left': this.offsetLeft-(description.offsetWidth-this.offsetWidth)/2,
-                    'opacity': 1
-                });
+            menuBtns[i].addEventListener('mouseenter', function(e) {
+                thisBtn = this;
+                // Delay showing description by 1 second (if user is not sure, they will hover longer)
+                timerToWaitForUser = window.setTimeout(function(){
+                    // Get data for the menu element to use as description
+                    description.textContent = thisBtn.getAttribute('data');
+                    // Position & show description
+                    description.style.visibility = 'visible'; // quickly show then...
+                    $(description).css({
+                        'left': thisBtn.offsetLeft-(description.offsetWidth-thisBtn.offsetWidth)/2,
+                        'opacity': 1
+                    });
+                }, 1000);
             }); 
             menuBtns[i].addEventListener('mouseleave', function() {
+                // Clear timeout
+                window.clearTimeout(timerToWaitForUser);
                 // Hide btn description
                 $(description).css({
                     'visibility': 'hidden',
-                    'left': 0,
                     'opacity': 0
                 });
             })
